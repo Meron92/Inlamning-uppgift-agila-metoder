4c3ed5333038c5f67bd69853d5dc6b8c7a8649bc
Meron92 <Meron_Mekonnen@hotmail.com>
2022-04-27 16:51:00 +0200

    #11 Skapa sökfunktion

--- src/Pages/ChosenInfo.jsx
+++ src/Pages/ChosenInfo.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import React, { useContext, useState } from "react";
 import Info from "../Components/Info";
 import { userContext } from "../Components/Context";
 
@@ -6,14 +6,29 @@ import { userContext } from "../Components/Context";
 
 const ChosenInfo = () => {
     const { infos } = useContext(userContext)
-    
+    const [searchInfo, setSearchInfo] = useState([])
+
+    function search (e) {
+        setSearchInfo(e.target.value)
+    }
     return ( 
         <section className="wrapperInfo">
             <h2>SCRUM GUIDE</h2>
+            <input className="input" type="text" onChange={search} />
            
-           { infos.map((info, i) => (
-    <section className="borderInfo">
-                <Info infos={info} img={info.img} titel={info.titel} desc={info.desc} key={i}/>
+           {
+              infos.filter((info)=> {
+                          
+                if (searchInfo == ""){
+                     return infos
+                    }
+                     else if (info.titel.toLowerCase().includes(searchInfo.toLowerCase())){
+                     return info.titel }
+           
+                    })
+                    .map((info) => (
+    <section className="borderInfo"key={info.id}>
+                <Info infos={info} img={info.img} titel={info.titel} desc={info.desc} />
                 </section>
               
             ))}
@@ -21,4 +36,4 @@ const ChosenInfo = () => {
      )
 }
 
-export default ChosenInfo 
+export default ChosenInfo; 
